8281f4ad6461c00d0a59b495562ab71e2154b8da
Tommy Choe <tchoe1@umbc.edu>
2015-12-23 18:10:05 -0500

    Setup NavigationController
    Initial setup for NavigationController. Color and style changes to
    UIStatusBar, UINavigationBar, and UINavigationController.

---
+++ LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  GymBuddy
+//
+//  Created by Tommy Choe on 12/23/15.
+//  Copyright © 2015 Tommy Choe. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        self.navigationItem.title = "Sign In"
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Log in", style: UIBarButtonItemStyle.Plain, target: self, action: nil)
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
