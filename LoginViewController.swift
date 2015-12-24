a4ae6a1d7fadf5be624d2dd7475916b8431e1497
Tommy Choe <tchoe1@umbc.edu>
2015-12-24 13:03:28 -0500

    Register button and editTextFields

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -13,11 +13,25 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        self.configView()
+    }
+    
+    //MARK: - Helper Methods
+    func configView(){
+        
+        //Setup navigationItem for ViewController
         self.navigationItem.title = "Sign In"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Log in", style: UIBarButtonItemStyle.Plain, target: self, action: nil)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Log in", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("logInUser"))
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: "Register", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("registerNewUser"))
     }
 
-
+    func logInUser(){
+        print("Verifying user credentials...")
+    }
+    
+    func registerNewUser(){
+        print("Register new user in process...")
+    }
 }
 
 
